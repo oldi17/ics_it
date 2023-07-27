e9dd0b5731d0ede89d9a134944b4dc913967c331
oldi <odi.scs.15@gmail.com>
2023-07-27 10:13:44 +0300

    2.2 created table dbo.Family

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -3,4 +3,11 @@ create table dbo.SKU(
     ,Code as 's' + cast(ID as nvarchar)
     ,Name nvarchar(40) NOT NULL
     ,CONSTRAINT UNQ_SKU_Code UNIQUE(Code)
-)
+);
+
+create table dbo.Family(
+    ID int NOT NULL IDENTITY
+    ,SurName nvarchar NOT NULL
+    ,BudgetValue money NOT NULL
+);
+
