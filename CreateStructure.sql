1bc87a65cc8c1ab497f9c82107366db73ffbde9b
oldi <odi.scs.15@gmail.com>
2023-07-27 10:44:23 +0300

    2.3 created table dbo.Basket and corrected minor mistakes

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -1,13 +1,28 @@
 create table dbo.SKU(
-    ID int NOT NULL IDENTITY
+    ID int NOT NULL IDENTITY PRIMARY KEY
     ,Code as 's' + cast(ID as nvarchar)
     ,Name nvarchar(40) NOT NULL
     ,CONSTRAINT UNQ_SKU_Code UNIQUE(Code)
 );
 
 create table dbo.Family(
-    ID int NOT NULL IDENTITY
-    ,SurName nvarchar NOT NULL
+    ID int NOT NULL IDENTITY PRIMARY KEY
+    ,SurName nvarchar(40) NOT NULL
     ,BudgetValue money NOT NULL
 );
 
+create table dbo.Basket(
+    ID int NOT NULL IDENTITY PRIMARY KEY
+    ,ID_SKU int NOT NULL
+    ,ID_Family int NOT NULL
+    ,Quantity int NOT NULL
+    ,Value money NOT NULL
+    ,PurchaseDate datetime NOT NULL DEFAULT(SYSDATETIME())
+    ,DiscountValue numeric(4,3) NOT NULL
+    ,CONSTRAINT CHK_Basket_Quantity CHECK(Quantity >= 0)
+    ,CONSTRAINT CHK_Basket_Value CHECK(Value >= 0.00)
+    ,CONSTRAINT FK_Basket_SKU FOREIGN KEY(ID_SKU)
+        REFERENCES dbo.SKU(ID)
+    ,CONSTRAINT FK_Basket_Family FOREIGN KEY(ID_Family)
+        REFERENCES dbo.Family(ID)
+);
